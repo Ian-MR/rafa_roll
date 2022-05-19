8118f50052e4dd698b0da63512748126a0b5a6b1
Ian <74927798+Ian-MR@users.noreply.github.com>
2022-05-19 15:31:07 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-var contentBox = document.querySelector(".box-container")
-window.addEventListener('click',() =>{
-    contentBox.innerHTML = '<iframe id="video" width="640" height="360" src="https://www.youtube.com/embed/hZQAtt7z89g?autoplay=1&loop=1&controls=0&modestbranding=1" allow="autoplay" title="YouTube video player" frameborder="0"></iframe>'
-})
