b105d0bdafd86514b0e6a317000c3d2ca86d56df
Ian <74927798+Ian-MR@users.noreply.github.com>
2022-05-19 15:18:29 -0300

    v2.0

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //hZQAtt7z89g, p4aPlYN6x1Q
+var contentBox = document.querySelector(".box-container")
+window.addEventListener('click',() =>{
+    contentBox.innerHTML = '<iframe id="video" width="640" height="360" src="https://www.youtube.com/embed/hZQAtt7z89g?autoplay=1&loop=1&controls=0&modestbranding=1" allow="autoplay" title="YouTube video player" frameborder="0"></iframe>'
+})
